a49d63d6e3feaf3cd2f2cc5ae6bd9722f1d80813
Pratyush Birole  ▲ <birole.pratyush@gmail.com>
2024-12-23 16:46:27 +0530

    Add files via upload

---
+++ BinarySearch/BS24ZscalerOA.java
@@ -0,0 +1,5 @@
+public class BS24ZscalerOA {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ BinarySearch/BS25AmazonOA.java
@@ -0,0 +1,31 @@
+public class BS25AmazonOA {
+
+    public static void main(String[] args) {
+        int nums [] = {1,3,4,4,5};
+        int k = 2;
+        int maxAns = Integer.MIN_VALUE;
+         
+        for(int i = 0 ; i< nums.length ; i++){
+            maxAns = Math.max(maxAns,nums[i]);
+        }
+        int start= maxAns;
+        int end = maxAns+k;
+        int result = 0;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(canDisttrubute(nums,mid,k)){
+                result= mid;
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        System.out.println(result);
+    }public static boolean canDisttrubute(int nums [] , int mid , int k){
+        int required = 0;
+        for(int value:nums){
+            required +=(mid-value);
+        }
+        return required>=k;
+    }
+}
